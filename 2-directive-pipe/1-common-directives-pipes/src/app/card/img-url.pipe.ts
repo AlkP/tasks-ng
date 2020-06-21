fd4efd7a5fdfe0f86c2f9d9129c47aad5e649350
Alexey Pasenko <avpasenko@dasreda.ru>
2020-06-21 16:32:56 +0300

    Module 2 homework

--- 2-directive-pipe/1-common-directives-pipes/src/app/card/img-url.pipe.ts
+++ 2-directive-pipe/1-common-directives-pipes/src/app/card/img-url.pipe.ts
@@ -4,7 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'imgUrl',
 })
 export class ImgUrlPipe implements PipeTransform {
-  transform(): string {
-    return ``;
+  transform(images: any): string {
+    if (!images) {
+      return ``;
+    } else {
+      return images[0]?.url
+    }
   }
 }
